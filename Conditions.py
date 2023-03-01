dc08bfad835b041281a2a7c3017234c273b23b07
Baha Artaimi <82269090+Baha-Rtemi@users.noreply.github.com>
2023-03-01 23:37:23 +0100

    Remove unusable comment

--- Conditions.py
+++ Conditions.py
@@ -2,10 +2,6 @@ print("# THIS IS PRINT FUNCTION")
 print("Hello, World")
 print()
 print("# THIS IS VARIABLES:")
-# print(f"a = 28 <==> int"\n + 'b = 1.5 <==> float'\n + "c = c <==> chr: A Single character"\n
-#       "d = Baha <==> str"\n
-#       "e = True / False <==> bool"\n
-#       "f = None <==> NoneType A special value ( None ) indicating the absence of a value."\n)
 print(""" a = 28             <==>     int
  b = 1.5            <==>     float
  c = c              <==>     chr: A Single character
