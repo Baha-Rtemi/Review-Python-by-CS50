ba666741ed1f0530754cae3c68e1923d40220e31
Baha Artaimi <bahaalrtemi@gmail.com>
2023-03-02 01:37:05 +0100

    Add String

---
+++ Sequences-Of-Data.py
@@ -0,0 +1,9 @@
+# (( Mutable )) means that once a sequence has been defined,
+# we can change individual elements of that sequence,
+# and (( Ordered )) means that the order of the objects matters.
+
+# 1-    Strings:
+#           Ordered: Yes
+#           Mutable: No
+Name = "Baha"
+print(Name[0], Name[1], Name[2], Name[3])
