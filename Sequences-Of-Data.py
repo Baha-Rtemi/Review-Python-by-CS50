2b18b2a5e15dec1fbe64fc04cccac2aa258ad61a
Baha Artaimi <bahaalrtemi@gmail.com>
2023-03-02 01:49:02 +0100

    change verabile name from Name to String

--- Sequences-Of-Data.py
+++ Sequences-Of-Data.py
@@ -1,3 +1,6 @@
+# Just empty Speace from up
+print()
+
 # (( Mutable )) means that once a sequence has been defined,
 # we can change individual elements of that sequence,
 # and (( Ordered )) means that the order of the objects matters.
@@ -5,5 +8,8 @@
 # 1-    Strings:
 #           Ordered: Yes
 #           Mutable: No
-Name = "Baha"
-print(Name[0], Name[1], Name[2], Name[3])
+String = "Baha"
+print(String[0], String[1], String[2], String[3])
+
+# Just empty Speace from down
+print()
