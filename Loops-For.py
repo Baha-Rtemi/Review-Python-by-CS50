8e2af8db45cab2340bbb78647e6f4ab6c69a9627
Baha Artaimi <bahaalrtemi@gmail.com>
2023-03-16 17:53:38 +0100

    add codes to Loops-For.py

--- Loops-For.py
+++ Loops-For.py
@@ -9,8 +9,35 @@
 # 	if x == "Apoled":
 #       break
 # 	  print(x)
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x) 
-  if x == "banana":
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x) 
+#   if x == "banana":
+#     break
+###############################################
+name = "Baha"
+for char in name:
+  print(char)
+print("#"*10, end="\n\n")
+names = ["Baha","Ali","Abdalh","Apoled"]
+for name in names:
+  # printout all names
+  print(name)
+print("#"*10, end="\n\n")
+# printout names without what's after "Ali" name
+for SpecialName in names:
+  print(SpecialName)
+  if SpecialName == "Ali":
     break
+print("#"*10, end="\n\n")
+# printout names without "Ali" name and what's after it
+for SpecialNAME in names:
+  if SpecialNAME == "Ali":
+    break
+  print(SpecialNAME)
+print("#"*10, end="\n\n")
+# printout all names without "Ali" name
+for name in names:
+  if name == "Ali":
+    continue
+  print(name)
