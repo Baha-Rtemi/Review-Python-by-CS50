39d926989f790fe1e352291ce4a07783a4145200
Baha Artaimi <bahaalrtemi@gmail.com>
2023-03-16 22:51:53 +0100

    create funcations.py & square.py

---
+++ square.py
@@ -0,0 +1,5 @@
+from funcations import square
+# import funcations
+for i in range(11):
+    print(f"The Square of Number {i} is {square(i)}")
+    # print(f"The Square of Number {i} is {funcations.square(i)}")
