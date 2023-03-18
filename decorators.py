22d9d10781e081b931ceecd2208f583de9316191
Baha Artaimi <bahaalrtemi@gmail.com>
2023-03-18 20:15:25 +0100

    more review about decorators

--- decorators.py
+++ decorators.py
@@ -1,12 +1,19 @@
-
+# announce a function hello() (( which mean make something before & after the function work ))
 def announce(f):
+    # define wrapper function
     def wrapper():
-        print("The funcation about to run")
+        print("before the function is running ...  ...  ... ".title())
         f()
-        print("The funcation done running")
+        print(" ...  ...  ... after the function is running.".title())
+    # return wrapper function
     return wrapper
-@announce
+# announce a function before it's start
+# function hello()
+@announce # to announce the function hello()
 def hello():
-    print("Hello, World!")
+    print(">>>   the function is running.   <<<".upper())
 
+# but there is something will happpen before it.
+# call the function hello()
 hello()
+# also there is something will happpen afetr it.
